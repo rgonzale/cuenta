239f1709627c217bf116114a890d60f6b16cfe17
rgonzale <rgonzale@kronos.(none)>
2013-12-13 11:49:47 -0600

    Another fresh commit

--- functions.c
+++ functions.c
@@ -36,17 +36,22 @@ void die() {
 char * parse(int *argc, char **argv) {
 
 	int i;
-	char s[BUFSIZE];
+	char s[1];
 
-	if ((*argc == 2) && ((*argv[1] == 'S') || (*argv[1] == 's')))
-		print_summary();
+	if (((*argc == 2) || (*argc == 3)) && ((*argv[1] == 'S') || (*argv[1] == 's'))) {
+
+		if ((argv[2] != NULL) && (strchr("ibefmIBEFM", (*argv[2])) == NULL))
+			print_summary(NULL);
+		else 
+			print_summary(argv[2]);
+	}
 
 	else if ((*argc) == 4) {
 
 		if (strchr("ibefmIBEFM", (*argv[1])) == NULL) 
 			die();
 
-		if ((strncpy(s, argv[2], BUFSIZE)) == NULL)
+		if ((strncpy(s, argv[2], 1)) == NULL)
 			die();
 
 		for (i = 0; i < strlen(argv[2]); i++)
@@ -56,7 +61,7 @@ char * parse(int *argc, char **argv) {
 		}
 
 		snprintf(query, 64, "insert into acct values(CURDATE(), \"%s\", %d, \"%s\")"
-			, argv[1], atoi(argv[2]), argv[3]);
+				, argv[1], atoi(argv[2]), argv[3]);
 
 		return query;
 	}
@@ -76,18 +81,30 @@ int get_month() {
 	return tm.tm_mon + 1;
 }
 
-int print_summary() {
+int print_summary(char *category) {
+
 	printf("Printing Summary\n");
+
 	mysql_start();
 
-	mysql_select(con, "select day, category, format(amount/100,2), description from acct");
+	if (category == NULL) {
 
-	printf("Balance: ");
+		mysql_select(con, "select day, category, format(amount/100,2), description from acct");
 
-    mysql_select(con, "select format((select sum(amount/100) from acct where category = 'I')"
-		"- (select sum(amount/100) from acct where category REGEXP '[BEFM]'),2);");
+		printf("Balance: ");
 
-	mysql_stop();
+		mysql_select(con, "select format((select sum(amount/100) from acct where category = 'I')"
+				"- (select sum(amount/100) from acct where category REGEXP '[BEFM]'),2);");
 
+		mysql_stop();
+	}
+	else {
+
+		snprintf(query, 128, "select day, category, format(amount/100,2), description from acct where"
+				" category = '%s'", category);
+
+		mysql_select(con, query);
+
+	}
 	return 0;
 }
--- prototypes.h
+++ prototypes.h
@@ -1,4 +1,4 @@
 char * parse(int *argc, char **argv);
 void die();
 int get_month();
-int print_summary();
+int print_summary(char *category);
